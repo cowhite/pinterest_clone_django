43125bbe7b738c694d588ef110e42ba126cb02c4
Bhaskar <bhaskar@cowhite.com>
2014-09-22 00:38:47 +0530

    TC-9 Shows pin thumbs on board in userprofile

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -8,13 +8,14 @@ from util.utils import get_content_type
 register = template.Library()
 
 @register.inclusion_tag("inclusion_tags/is_following.html")
-def is_following(user, object):
+def is_following(user, object, col):
     content_type_obj = get_content_type(object)
     return {
                 "user": user,
                 "content_type": content_type_obj,
                 "object": object,
-                "is_following": user.is_following(content_type_obj.id, object.id)
+                "is_following": user.is_following(content_type_obj.id, object.id),
+                "col": col
            }
 
 
